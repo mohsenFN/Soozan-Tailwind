238725aa5dbd402e1d4902055badb386a1995e22
none <maybeevmo@gmail.com>
2024-02-12 00:57:21 -0800

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      body: ['iranyekanBakh', 'sans-serif'],
+      display: ['iranyekanBakh', 'sans-serif'],
+    },
+  },
+  plugins: [],
+}
+
